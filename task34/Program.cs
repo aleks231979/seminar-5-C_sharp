e687d603b788975745fb5b331e8c7a2daeb8d524
андрей <p.aleksey2010@yandex.ru>
2022-09-28 19:30:28 +0300

    .\Program.cs

---
+++ task34/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// Например:
+// [345, 897, 568, 234] -> 2
+
+int[] GetArray(int size, int leftRange, int rightRange) // метод заполнения массива случайными числами в диапазоне от leftRange до rightRange
+{
+    int[] arr = new int[size]; // создаем новый массив размером size
+    Random rand = new Random(); // создали переменную рандома rand
+    for (int i = 0; i < size; i++) // циклом заполняем наш массив arr случайными цифрами в промежутке от leftRang до rightRange
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr; // возвращаем из метода заполненный массив arr
+}
+
+int numberOfEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) // проверка на четность элемента массива
+        {
+            count = count + 1; // считаем количество четных элементов в массиве
+        }
+    }
+    return count;
+}
+
+int[] array = GetArray(4, 100, 1000);
+Console.WriteLine(string.Join(", ", array));
+int count = numberOfEvenNumbers(array);
+Console.WriteLine(count);
